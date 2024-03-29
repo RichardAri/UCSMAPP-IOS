4b13c695f19414417312e971056d0cd7a5e69997
RichardAri <richardaripari@gmail.com>
2024-03-29 00:06:27 -0500

    Initial Commit

---
+++ UCSM-IOS/UCSM_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCSM_IOSApp.swift
+//  UCSM-IOS
+//
+//  Created by Richard on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct UCSM_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
